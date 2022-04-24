2ec75b30a138718a9e7c19d91c210c3a80f4515a
Kei Nakamatsu <kei.nakamatsu@askul.com>
2022-04-24 17:27:53 +0900

    なんとなく挙動整理できてきた

--- src/main/kotlin/com/example/demo/MainApplication.kt
+++ src/main/kotlin/com/example/demo/MainApplication.kt
@@ -5,31 +5,41 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 
 
-suspend fun main(args: Array<String>) {
-	System.out.println("test0")
+suspend fun main() {
 	try {
-		System.out.println("test1")
-		failedConcurrentSum()
-		System.out.println("test2")
-	} catch(e: ArithmeticException) {
-		println("test7. Computation failed with ArithmeticException")
-		System.out.println("test3")
+		println("1: start main method.")
+		var retValue = coroutineLearnSample()
+		println("7: retValue")
+		println(retValue)
+	} catch(e: Exception) {
+		println("throw exception.")
+	} finally {
+	    println("8: finally call on main method.")
 	}
-	System.out.println("test4")
 }
 
-suspend fun failedConcurrentSum(): Int = coroutineScope {
-	val one = async<Int> {
+// NOTE: 参考 https://kotlinlang.org/docs/composing-suspending-functions.html#structured-concurrency-with-async
+suspend fun coroutineLearnSample(): Int = coroutineScope {
+	val one = async {
 		try {
-			delay(Long.MAX_VALUE) // Emulates very long computation
-			42
+			delay(3000L) // Emulates very long computation
+			10
 		} finally {
-			println("test5. First child was cancelled")
+			println("5. call 'one', after 'two' method.")
 		}
 	}
-	val two = async<Int> {
-		println("test6. Second child throws an exception")
-		throw ArithmeticException()
+	val two = async {
+		println("3: call 'two' method.")
+		20
 	}
-	one.await() + two.await()
+	println("2: before calling coroutine await.")
+	// NOTE: one.await() したからといって後続の two.await() がシーケンシャルに実行されるわけじゃないのが罠
+	// ( JavaScript の async/await 的な挙動イメージだと勘違いしてしまう ）
+	return@coroutineScope one.await().let {
+		var twoRet = two.await()
+		println("4: completed 'two' method.")
+		var ret = twoRet + it
+		println("6: return sum of each coroutine return.")
+		ret
+	}
 }
